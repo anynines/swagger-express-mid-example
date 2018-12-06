91775948343952095d68e477b43cd7d1a43db958
Pierre-Nicolas <arkanoryn@gmail.com>
2018-12-06 07:00:34 +0100

    WIP - working state
    Signed-off-by: Pierre-Nicolas <arkanoryn@gmail.com>

--- server-advanced.js
+++ server-advanced.js
@@ -11,6 +11,9 @@ const swagger = require('swagger-express-middleware');
 const Middleware = swagger.Middleware;
 const MemoryDataStore = swagger.MemoryDataStore;
 const Resource = swagger.Resource;
+const mongo = require('mongodb');
+const monk = require('monk');
+const db = monk('localhost:27017/mockers');
 
 let app = express();
 let middleware = new Middleware(app);
@@ -19,24 +22,28 @@ let middleware = new Middleware(app);
 let swaggerFile = path.join(__dirname, 'spec.yaml');
 middleware.init(swaggerFile, (err) => {
 
-  // Create a custom data store with some initial mock data
+  // // Create a custom data store with some initial mock data
   let myDB = new MemoryDataStore();
   myDB.save(
-    new Resource('/pets/Snoopy', { name: 'Snoopy', type: 'dog', tags: ['black', 'white']})
+    new Resource('/pets/Snoopy', { name: 'Snoopy', type: 'dog', tags: ['black', 'white', 'purple', 'yellow'] })
   );
+  // Make our db accessible to our router
+  app.use(function (req, res, next) {
+    req.db = db;
+    next();
+  });
 
-  app.use(middleware.metadata());
-  app.use(middleware.files());
-  app.use(middleware.parseRequest());
-
-  // These two middleware don't have any options (yet)
   app.use(
+    middleware.metadata(),
+    middleware.files(),
+    middleware.parseRequest(),
     middleware.CORS(),
     middleware.validateRequest()
   );
 
   // Add custom middleware
-  app.get('/pets', (req, res, next) => {
+  app.get('/rifability/vin', (req, res, next) => {
+    console.log('tmp xxxxxxxxxxxxxxxxxxxxx')
     if (process.env.MOCK === 'true') {
       res.status(200).json([]);
     }
@@ -49,6 +56,11 @@ middleware.init(swaggerFile, (err) => {
   // The mock middleware will use our custom data store,
   // which we already pre-populated with mock data
   app.use(middleware.mock(myDB));
+  app.use((err, req, res, next) => {
+    res.status(err.status);
+    res.type('html');
+    res.send(util.format('<html><body><h1>%d Error!</h1><p>%s</p></body></html>', err.status, err.message));
+  });
 
   app.listen(8000, () => {
     console.log('The Swagger Pet Store is now running at http://localhost:8000');
